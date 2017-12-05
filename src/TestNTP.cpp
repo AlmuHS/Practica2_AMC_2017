a1407a75b1f770d7f6b918bf5551400203a1437b
AlmuHS <liberamenso10000@gmail.com>
2017-12-05 13:13:54 +0100

    Changes origin on datafile in TestNTP::testFromFile()

--- src/TestNTP.cpp
+++ src/TestNTP.cpp
@@ -9,7 +9,8 @@ TestNTP::TestNTP()
     //ctor
 }
 
-void TestNTP::testRandom(){
+void TestNTP::testRandom()
+{
     int size = 0;
 
     std::cout<<"Set size: ";
@@ -25,46 +26,48 @@ void TestNTP::testRandom(){
     double min_distance = NTP.simpleSolution(p1, p2, p3);
 
     std::cout<<"The minimal distance is: "<<min_distance<<std::endl
-            <<"The minimal trio is <"<<p1.first<<", "<<p1.second<<"> "
-            <<"<"<<p2.first<<", "<<p2.second<<"> "
-            <<"<"<<p3.first<<", "<<p3.second<<">"<<std::endl;
+             <<"The minimal trio is <"<<p1.first<<", "<<p1.second<<"> "
+             <<"<"<<p2.first<<", "<<p2.second<<"> "
+             <<"<"<<p3.first<<", "<<p3.second<<">"<<std::endl;
 
 }
 
-void TestNTP::testFromFile(){
+void TestNTP::testFromFile()
+{
     int option = 0;
     NodeSet NS;
+    std::string filename = "";
 
-    std::vector<std::string> file = {"data/berlin52.tsp", "data/ch130.tsp", "data/d493.tsp"};
+    std::vector<std::string> file = {"data/berlin52.tsp/berlin52.tsp", "data/ch130.tsp/ch130.tsp", "data/ch150.tsp/ch150.tsp"};
     std::pair<float, float> p1, p2, p3;
 
     std::cout<<"Select file: \n"
              <<"1. berlin52\n"
              <<"2. ch130\n"
-             <<"3. d493\n"
+             <<"3. ch150\n"
              <<"4. Other file\n"
              <<"Select option: ";
     std::cin>>option;
 
-    if(option != 4) NS = GenNS.genNodeSetFromFile(file[option-1]);
-    else{
-        std::string filename = "";
+    if(option != 4)
+        filename =  file[option-1];
+    else
+    {
         std::cout<<"Introduce filename: ";
         std::cin>>filename;
 
-        filename = "data/" + filename;
-
-        NS = GenNS.genNodeSetFromFile(filename);
+        filename = "data/" + filename + "/" + filename;
     }
 
+    NS = GenNS.genNodeSetFromFile(filename);
     NS.showNodeSet();
 
     NearestTrioProblem NTP(NS);
     double min_distance = NTP.simpleSolution(p1, p2, p3);
 
     std::cout<<"The minimal distance is: "<<min_distance<<std::endl
-            <<"The minimal trio is <"<<p1.first<<", "<<p1.second<<"> "
-            <<"<"<<p2.first<<", "<<p2.second<<"> "
-            <<"<"<<p3.first<<", "<<p3.second<<">"<<std::endl;
+             <<"The minimal trio is <"<<p1.first<<", "<<p1.second<<"> "
+             <<"<"<<p2.first<<", "<<p2.second<<"> "
+             <<"<"<<p3.first<<", "<<p3.second<<">"<<std::endl;
 
 }
