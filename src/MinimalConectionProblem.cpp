b1ac73da0c5ccfb3f51e6b84e1dc2472bbf327d0
AlmuHS <liberamenso10000@gmail.com>
2017-12-30 00:05:30 +0100

    anadida licencia

--- src/MinimalConectionProblem.cpp
+++ src/MinimalConectionProblem.cpp
@@ -1,3 +1,20 @@
+/*Copyright 2017 Almudena Garcia Jurado-Centurion
+
+This file is part of Practica2_AMC.
+
+Practica2_AMC is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Practica2_AMC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Practica2_AMC.  If not, see <http://www.gnu.org/licenses/>.*/
+
 #include "MinimalConectionProblem.h"
 
 MinimalConectionProblem::MinimalConectionProblem()
