889c691a501e564b9a0dae59a695311905ea5b90
AlmuHS <liberamenso10000@gmail.com>
2018-01-05 18:25:06 +0100

    pequenas optimizaciones

--- src/MinimalConectionProblem.cpp
+++ src/MinimalConectionProblem.cpp
@@ -23,7 +23,8 @@ along with Practica2_AMC.  If not, see <http://www.gnu.org/licenses/>.*/
 #include <algorithm>
 
 //overloaded operator, needed to std::set and std::sort
-bool operator<(const edge& e1, const edge& e2){
+bool operator<(const edge& e1, const edge& e2)
+{
     return(e1.distance < e2.distance);
 }
 
@@ -75,7 +76,8 @@ int MinimalConectionProblem::kruskalSolution(std::set<edge>& solution)
     std::sort(EdgeSet.begin(), EdgeSet.end());
 
     //Initialize set vector
-    for(NodeSet::iterator it = _NS.begin(); it != _NS.end(); it++){
+    for(NodeSet::iterator it = _NS.begin(); it != _NS.end(); it++)
+    {
         std::set<std::pair<float, float> > new_set;
         new_set.insert(*it);
         set_collection.push_back(new_set);
@@ -83,29 +85,32 @@ int MinimalConectionProblem::kruskalSolution(std::set<edge>& solution)
 
     //execute algorithm
     std::vector<edge>::iterator it = EdgeSet.begin();
-    while(connected < _NS.size()){
+    while(connected < _NS.size())
+    {
         int U, V;
         U = V = -1;
 
         //Search edge in set vector
         int i = 0;
-        while(U == -1 || V == -1){
-            if(U == -1){
-                if(set_collection[i].find(it->a) != set_collection[i].end()) U = i;
-            }
-            if(V == -1){
-                if(set_collection[i].find(it->b) != set_collection[i].end()) V = i;
+        while(U == -1 || V == -1)
+        {
+            if(!set_collection[i].empty())
+            {
+                if(U == -1 && set_collection[i].find(it->a) != set_collection[i].end()) U = i;
+                if(V == -1 && set_collection[i].find(it->b) != set_collection[i].end()) V = i;
             }
             i++;
         }
 
         //If both set are different, add edge to solution set
-        if(set_collection[U] != set_collection[V]){
+        if(set_collection[U] != set_collection[V])
+        {
             connected += 2;
 
             //Merge set
             std::set<std::pair<float, float> >::iterator itsc = set_collection[V].begin();
-            while(!set_collection[V].empty()){
+            while(!set_collection[V].empty())
+            {
                 set_collection[U].insert(*itsc);
                 itsc = set_collection[V].erase(itsc);
             }
